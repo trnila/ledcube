9d830c3e34f5be4e579d2a739328953e590c971c
Daniel Trnka <daniel.trnka@gmail.com>
2018-06-23 12:47:11 +0200

    mapped_file: raise error

--- sim/mapped_file.cpp
+++ sim/mapped_file.cpp
@@ -5,21 +5,26 @@
 #include <fcntl.h>
 #include <cerrno>
 #include <string>
+#include <stdexcept>
+#include <cstring>
 #include "mapped_file.h"
 
 MappedFile::MappedFile(const char *path) {
 	int fd = open(path, O_RDONLY);
 	if (fd <= 0) {
-		perror("open");
+		throw std::runtime_error(strerror(errno));
 	}
 
 	struct stat statbuf{};
 	if (fstat(fd, &statbuf) < 0) {
-		perror("fstat error");
+		throw std::runtime_error(strerror(errno));
 	}
 
 	len = (size_t) statbuf.st_size;
 	content = (char *) mmap(nullptr, len, PROT_READ, MAP_SHARED, fd, 0);
+	if (!content) {
+		throw std::runtime_error("Could not map file!");
+	}
 }
 
 MappedFile::~MappedFile() {
